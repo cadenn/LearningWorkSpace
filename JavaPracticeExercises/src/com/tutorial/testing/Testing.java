f87a2d4a3bf0945c4319c18c27000dd4ba442eeb
Ben Tran <bbq-ben@hotmail.com>
2015-06-06 02:44:40 -0400

    Partial implementation of greeting users

--- JavaPracticeExercises/src/com/tutorial/testing/Testing.java
+++ JavaPracticeExercises/src/com/tutorial/testing/Testing.java
@@ -1,5 +1,7 @@
 package com.tutorial.testing;
 
+import java.util.*;
+
 public class Testing {
 
 	/**
@@ -7,7 +9,15 @@ public class Testing {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		
+		String usersName = "";
+		
+		Scanner user_input = new Scanner (System.in);
+		System.out.println("Please enter your name?");
+		
+		usersName = user_input.next();
+		
+		System.out.println("Hello + " + usersName + "!");
 	}
 
 }
