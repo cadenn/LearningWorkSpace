4fb328ddbbdcaab8c3cceb2a9c44e065216b9f2e
Ben Tran <bbq-ben@hotmail.com>
2015-06-09 02:21:41 -0400

    Created the package to start the Lists, Strings execises.

---
+++ JavaPracticeExercises/src/listsStrings/Exercise1LargestInList.java
@@ -0,0 +1,17 @@
+package listsStrings;
+
+import java.util.*;
+
+public class Exercise1LargestInList {
+//1. Write a function that returns the largest element in a list.
+//This program will be written using a menu.
+	
+	
+	
+	public static void main(String args[]){
+
+		
+		
+	}
+	
+}
